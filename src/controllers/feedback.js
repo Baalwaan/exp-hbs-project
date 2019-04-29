b94f586640499974938aec12422095ed1ee2b2fb
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-04-29 14:04:23 +0100

    form data can be full extracted now

--- src/controllers/feedback.js
+++ src/controllers/feedback.js
@@ -1,6 +1,11 @@
 const feedback = (req, res) => {
   console.log("This is the >>>>", req.body["fav-exercise"]);
-  res.end();
+  res.render("feedback", {
+    name: req.body.name,
+    gym: req.body.gym,
+    duration: req.body.duration,
+    "fav-exercise": req.body["fav-exercise"]
+  });
 };
 
 module.exports = feedback;
