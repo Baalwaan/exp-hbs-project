7dbf63d8717c10fec329028c7b4483f81617da24
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-04-30 17:44:11 +0100

    timestamp added to db

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -6,10 +6,12 @@ const router = express.Router();
 const home = require("./home");
 const feedback = require("./feedback");
 const feedbackHistory = require("./feedbackhistory");
+const error = require("./deletefeedback.js");
 const error = require("./error.js");
 
 router.get("/", home);
 router.get("/feedbackhistory/:name", feedbackHistory);
+router.get("/delete/:name", deleteFeedback);
 router.post("/feedback", feedback);
 
 module.exports = router;
--- src/model/dbuild.sql
+++ src/model/dbuild.sql
@@ -8,7 +8,7 @@ create table gym (
 	gym_name VARCHAR(100) NOT NULL,
 	workout_duration VARCHAR(20) NOT NULL,
     favourite_workout VARCHAR(50) NOT NULL,
-	time_stamp VARCHAR(1000) NOT NULL)
+	time_stamp VARCHAR(100)
 );
 insert into gym (name, gym_name, workout_duration, favourite_workout, time_stamp) values ('Merola', 'Pure Gym', '45-60 Min', 'Chest Press', now()),('Mesut', 'Northampton leisure center', '30-45 Min', 'Arms', now()),('Abid', 'Virgin Gym', 'over 1.5Hr', 'Squats', now());
 
